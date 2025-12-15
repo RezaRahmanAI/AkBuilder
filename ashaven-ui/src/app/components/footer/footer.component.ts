03db38b6128b834720423c315cf654ffc37cc783
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2025-12-15 15:53:41 +0600

    Revamp footer design

--- ashaven-ui/src/app/components/footer/footer.component.ts
+++ ashaven-ui/src/app/components/footer/footer.component.ts
@@ -1,23 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule, RouterLink, FormsModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-  email: string = '';
-  currentYear: number = new Date().getFullYear();
-
-  subscribe() {
-    if (this.email) {
-      console.log('Subscribed with email:', this.email); // Placeholder for subscription logic
-      this.email = ''; // Reset input
-    }
-  }
+  readonly currentYear: number = new Date().getFullYear();
 }
