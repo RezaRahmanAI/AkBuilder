cf96db9925d60953b973d616c23c02a76944c229
Rizve Rahman Reza <rizverahman.ai@gmail.com>
2025-12-23 16:34:53 +0600

    Remove unused API components

--- backend/Real_Estate.server.api/Data/ApplicationDbContext.cs
+++ backend/Real_Estate.server.api/Data/ApplicationDbContext.cs
@@ -20,19 +20,10 @@ namespace Real_Estate.server.api.Data
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectGallery> ProjectGalleries { get; set; }
         public DbSet<ProjectFeature> ProjectFeatures { get; set; }
-        public DbSet<Landowner> Landowners { get; set; }
-        public DbSet<GalleryImage> GalleryImages { get; set; }
-        public DbSet<Slider> Sliders { get; set; }
         public DbSet<Testimonial> Testimonials { get; set; }
-        public DbSet<Contactus> Contactus { get; set; }
-        public DbSet<Consultant> Consultants { get; set; }
 
         public DbSet<Team> Teams { get; set; }
-        public DbSet<Offer> Offers { get; set; }
-        public DbSet<CompanyInfo> CompanyInfos { get; set; }
-        public DbSet<Aboutus> Aboutus { get; set; }
         public DbSet<Blog> Blogs { get; set; }
-        public DbSet<Faq> Faqers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
